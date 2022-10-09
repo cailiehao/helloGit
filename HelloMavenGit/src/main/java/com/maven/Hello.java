33caf9c9c98c6c4346de828c0e3cdc9b3c196d5e
cailiehao <hanyuy@163.com>
2022-10-09 20:39:22 +0800

    更新内容

--- HelloMavenGit/src/main/java/com/maven/Hello.java
+++ HelloMavenGit/src/main/java/com/maven/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("创建分支");
         System.out.println("主干添加112233");
         System.out.println("分支添加445566");
+        System.out.println("在线更新");
     }
 }
