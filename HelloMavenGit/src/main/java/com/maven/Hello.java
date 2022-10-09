638149cec5ef7cd13edd50f7696ca952a0b7f71a
hao <hao@163.com>
2022-10-09 20:13:04 +0800

    测试分支

--- HelloMavenGit/src/main/java/com/maven/Hello.java
+++ HelloMavenGit/src/main/java/com/maven/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("这个是初始化提交");
         System.out.println("111");
+        System.out.println("创建分支");
     }
 }
